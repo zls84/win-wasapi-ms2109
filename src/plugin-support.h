dd61df0d293e315b1e537d69985b20852164f0c3
zls84 <zolcsieles@zolcsieles.hu>
2024-07-02 05:04:34 +0200

    Update copyright texts.

--- src/plugin-support.h
+++ src/plugin-support.h
@@ -1,6 +1,6 @@
 /*
-Plugin Name
-Copyright (C) <Year> <Developer> <Email Address>
+OBS Macrosilicon 2109 WASAPI plugin - Based on original OBS WASAPI plugin
+Copyright (C) 2024 zls84 zolcsieles@gmail.com
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
